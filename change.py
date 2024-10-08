3b715f991f29c1427d9b7d135b54528242929c53
Macarena Capra Prince <162723190+macaprynce@users.noreply.github.com>
2024-10-08 18:20:38 -0300

    Update change.py

--- change.py
+++ change.py
@@ -1,15 +1,15 @@
 def change():
-    expense = 23.75
-    money = 100
-    vuelto = money // expense
-    print("ingresar gasto:")
-    print(f"{expense})
-    print("dinero recibido")
-    print(f"{money})
-    print()
-    print("vuelto")
-    print()
-    print("pesos:")
-    print(int(money-expense))
-    print("centavos:")
-    print(int((float(money-expense)-int(money-expense))*100))
+    gasto = 23.75
+    dinero_recibido = 100
+    vuelto = dinero_recibido - gasto
+    pesos = int(vuelto)
+    centavos = int((vuelto - pesos) * 100)
+    print("Ingresar gasto:")
+    print(f"{gasto}")
+    print("Dinero recibido")
+    print(f"{dinero_recibido}")
+    print("\nVuelto\n")
+    print("Pesos:")
+    print(f"{pesos}")
+    print("Centavos:")
+    print(f"{centavos}")
